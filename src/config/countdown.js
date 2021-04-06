8a162e3358577cc695da61c5d3a967d677d45961
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-04-07 00:54:13 +0530

    audit and website ticker update

--- src/config/countdown.js
+++ src/config/countdown.js
@@ -1,12 +1,12 @@
-const countdownEpoch = 1617814800; // https://www.epochconverter.com/
+const countdownEpoch = 1617955200; // https://www.epochconverter.com/
 
 const countdown = {
-  title: "Whitelisting closes in",
-  href: "https://twitter.com/epnsproject/status/1377667249457094659?s=20",
+  title: "Whitelist Closed!",
+  href: "https://twitter.com/epnsproject/status/1379373647727534086?s=20",
   countdownEpoch: countdownEpoch,
   button: true,
-  buttonTitle: "Read about how to participate in EPNS Polkastarter Whitelist",
-  buttonText: "IDO Whitelist Opens!",
+  buttonTitle: "Read about how to participate in EPNS Polkastarter Whitelist by creating memes",
+  buttonText: "#PUSHMEME Contest 📣",
   hide: false,
 }
 
