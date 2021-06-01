c7923a5bea1ac9a4ae36755e880bdd938b0b0e0d
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-06-02 03:29:59 +0530

    Updated frontpage

--- src/config/countdown.js
+++ src/config/countdown.js
@@ -2,7 +2,7 @@ const countdownEpoch = 1618326000; // https://www.epochconverter.com/
 
 const countdown = {
   title: "Thanks for the $PUSH",
-  href: "https://info.uniswap.org/pair/0xAf31Fd9C3B0350424BF96e551d2D1264d8466205",
+  href: "https://app.uniswap.org/#/swap?inputCurrency=0xf418588522d5dd018b425e472991e52ebbeeeeee",
   countdownEpoch: countdownEpoch,
   button: true,
   buttonTitle: "Check $PUSH on Uniswap",
