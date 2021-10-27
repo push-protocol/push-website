8c935139e50d13aaa6344eb783a2f29b2721dd17
Jaf <jafetsc@gmail.com>
2021-10-27 17:00:58 -0600

    modify countdown announcement

--- src/config/countdown.js
+++ src/config/countdown.js
@@ -2,11 +2,11 @@ const countdownEpoch = 1631023200; //1678914000; // https://www.epochconverter.c
 
 const countdown = {
   title: "Push for Governance",
-  href: "#",
+  href: "https://medium.com/ethereum-push-notification-service/epns-governance-goes-live-lets-push-for-progressive-decentralized-governance-7448b58b89b4",
   countdownEpoch: countdownEpoch,
   button: true,
   buttonTitle: "Are you ready for Governance",
-  buttonText: "Incoming Announcement",
+  buttonText: "EPNS Governance is LIVE!",
   hide: false,
 }
 
