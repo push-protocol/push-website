aeee9624d15c94aee3b32c095751f477912122b5
rijusougata13 <rijusougata13@gmail.com>
2021-12-09 13:28:20 +0530

    gov hero section added

--- src/pages/Gov.js
+++ src/pages/Gov.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import {Section, SectionFS, SectionFSHero, Content, Item, ItemH, ItemBreak, WaveOuter, WaveInner, Arc, H1, H2, H3, Image, P, Span, Anchor, Button, Showoff, FormSubmision, Input, TextField,DottedSection} from 'components/SharedStyling';
-
+import {Section, SectionFS,SectionHero, SectionFSHero, Content, Item, ItemH, ItemBreak, WaveOuter, WaveInner, Arc, H1, H2, H3, Image, P, Span, Anchor, Button, Showoff, FormSubmision, Input, TextField,DottedSection} from 'components/SharedStyling';
+import Wave from 'react-wavify';
 
 const Gov=()=>{
     return(
-        <GovernanceSection>
+        <>
+          <SectionHero padding="50px 0px 80px 0px">
+        <Content className="contentBox">
+          <Item margin="0px 20px">
+            <H1>Governance</H1>
+            <Span color="#fff" margin="-20px 0 0 0" size="1rem">Defines Governance</Span>
+          </Item>
+        </Content>
+        <WaveOuter>
+          <WaveInner>
+            <Wave fill='#fff'
+              paused={true}
+              options={{
+                height: 20,
+                amplitude: 30,
+                speed: 0.35,
+                points: 3
+              }}
+            />
+          </WaveInner>
+        </WaveOuter>
+      </SectionHero>
           {/* Start What is Governance */}
-          <Section>
-          Governance
+          <Section theme="#fff" padding="0px 0px 50px 0px">
+                <Content className="contentBox">
+                    Governance Start
+                </Content>
           </Section>
           {/* End What is Governance */}
           {/* Start Future Governance */}
+          <Section theme="#fff" padding="0px 0px 50px 0px">
+                <Content className="contentBox">
+                  Future  Governance Start
+                </Content>
+          </Section>
           {/* End Future Governance */}
          {/* Start process Governance */}
           {/* End process Governance */}
@@ -24,11 +52,10 @@ const Gov=()=>{
              {/* Start faq Governance */}
           {/* End faq Governance */}
        
-        </GovernanceSection>
+        </>
+
     )
 }
 
-const GovernanceSection=styled.div`
-    height: 100vh;
-`;
+
 export default Gov;
