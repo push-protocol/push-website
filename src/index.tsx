d910f25d32e9c280e6524094d77e11dc7eae6ef7
subhranshudas <subhranshu.opt@gmail.com>
2022-09-18 11:58:29 +0530

    hashrouter for GH pages for now

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import * as dotenv from 'dotenv';
 import './index.css';
 import App from './App';
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
