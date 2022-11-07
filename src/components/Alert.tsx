7c1174deee8002fd40dfe552e33fdf6af64bf0d8
corlard3y <corlardey@gmail.com>
2022-11-07 17:29:30 +0100

    mobile view

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -21,19 +21,15 @@ const Alert = ({ hideAlert, isAlertVisible }) => {
     return(
         <Section>
         {isAlertVisible && 
-            (<AlertContainer>
-            <AlertContent>
+            (
+            <AlertContainer>
                 <AlertText>
                     {alertMessage} 
-                    <KnowMoreLink href={alertLink} target="_blank">Know more <FiArrowUpRight /></KnowMoreLink>
+                    <KnowMoreLink href={alertLink} target="_blank">Know more </KnowMoreLink>
+                    <FiArrowUpRight className='icon' />
                 </AlertText>
-            </AlertContent>
 
             <CancelIcon>
-                <KnowMoreLinkMobile href={alertLink} target="_blank">
-                    Know more
-                    <FiArrowUpRight />
-                </KnowMoreLinkMobile> 
                 <AiOutlineClose size="1.25rem" color='#7f7b80' className='icon' onClick={hideAlert} />
             </CancelIcon>
         </AlertContainer>)}
@@ -47,48 +43,34 @@ const AlertContainer = styled(ItemHV2)`
     background-color: black;
     padding: 1rem 0;
     width: 100%;
-    display:flex;
+    display: flex;
+    justify-content: center;
     @media ${device.tablet}{
         padding: 0rem 0rem;
-        display:none;
     }
 `
 
-const AlertContent = styled(ItemHV2)`
-    font-size: 1.125rem;
-    white-space:no-wrap;
-    @media ${device.tablet}{
-        text-align: left;
-        padding: 0.5rem 0.5rem;
-    }
-
-
-`
-const AlertText = styled.b`
-    font-family: 'Strawford';
-    font-style: normal;
+const AlertText = styled.div`
     font-weight: 500;
     font-size: 18px;
     line-height: 142%;
     letter-spacing: -0.03em;
     color: #FFFFFF;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+    margin: auto auto;
     @media ${device.tablet}{
-        flex-direction: column;
         font-size: 14px;
+        width: 80%;
+        padding: 10px;
+    }
+
+    .icon {
+        position: relative;
+        bottom: -3px;
     }
 `
 const CancelIcon = styled.div`
     margin-right: 20px;
     @media ${device.tablet}{
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0.5rem 0.5rem;
     }
 
     .icon{
@@ -99,32 +81,22 @@ const CancelIcon = styled.div`
     }
 `
 
-const KnowMoreLink = styled(Atag)`
+const KnowMoreLink = styled.a`
     padding: 0;
     background: none;
     font-size: 1.125rem;
     padding-left: 1rem;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
     &:hover {
         opacity: 0.75;
       }
 
       @media ${device.tablet}{
         font-size: 14px;
-        display: none;
+        padding-left: .5rem;
     }
 `
-
-const KnowMoreLinkMobile = styled(Atag)`
-    display: none;
-
-    @media ${device.tablet}{
-        display: flex;
-        padding: 0;
-        background: none;
-        font-size: 14px;
-    }`
 
 export default Alert;
