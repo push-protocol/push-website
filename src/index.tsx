055660e55f60e306fbec5cad948f20eff04ad3e7
corlard3y <corlardey@gmail.com>
2022-12-20 01:14:13 +0100

    push check

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -14,6 +14,12 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
