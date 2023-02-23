ffe7bf5de313c4fec79dcb482b0af8e12ba18aa7
corlard3y <corlardey@gmail.com>
2023-02-23 15:18:35 +0100

    fix hook

--- src/hooks/useOnScreen.ts
+++ src/hooks/useOnScreen.ts
@@ -1,10 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 /* eslint-disable */
-import { useState, useEffect, useRef, MutableRefObject  } from "react";
+import { useState, useEffect, useRef, MutableRefObject } from 'react';
 
 // Hook
-function useOnScreen<T extends Element>(ref: MutableRefObject<T>, rootMargin: string = "0px"): boolean {
+function useOnScreen<T extends Element>(ref: MutableRefObject<T>, rootMargin: string = '0px'): boolean {
   // State and setter for storing whether element is visible
   const [isIntersecting, setIntersecting] = useState<boolean>(false);
   useEffect(() => {
@@ -21,7 +21,9 @@ function useOnScreen<T extends Element>(ref: MutableRefObject<T>, rootMargin: st
       observer.observe(ref.current);
     }
     return () => {
-      observer.unobserve(ref.current);
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
     };
   }, []); // Empty array ensures that effect is only run on mount and unmount
   return isIntersecting;
