927a5f1fa3ee87a81a597e07a059fa0a6aefaae5
corlard3y <corlardey@gmail.com>
2023-06-12 10:09:45 +0100

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
