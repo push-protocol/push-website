b0e38c8b05f27709c6e70601edad946686e26805
corlard3y <corlardey@gmail.com>
2023-06-19 22:56:08 +0100

    make css changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,9 @@ function HomepageHeader() {
       <div className="section-container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className='spacing-small'>
+        <div className='spacing-small pointer'>
           <Link
-          className='hero__button'
+            className='hero__button'
             to="/docs/getting-started">
             Get Started
             <FiArrowUpRight className='ml-4' />
