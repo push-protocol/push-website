50c0876d08b7f1f7fff6890a507ff429d0544fe4
corlard3y <corlardey@gmail.com>
2023-07-10 13:23:44 +0100

    comment code

--- src/pages/BlogItem.tsx
+++ src/pages/BlogItem.tsx
@@ -25,11 +25,11 @@ import SpinnerSVG from 'assets/Spinner.gif';
 import { BsFillPlayCircleFill } from 'react-icons/bs'
 import Skeleton from 'react-loading-skeleton';
 import { AiOutlineClose } from 'react-icons/ai';
-import { ReactComponent as ModalTwitter } from 'assets/Modal-Twitter.svg';
+// import { ReactComponent as ModalTwitter } from 'assets/Modal-Twitter.svg';
 
-import { ReactComponent as ModalFacebook } from 'assets/Modal-Facebook.svg';
+// import { ReactComponent as ModalFacebook } from 'assets/Modal-Facebook.svg';
 
-import { ReactComponent as ModalLinkedIn } from 'assets/Modal-LinkedIn.svg';
+// import { ReactComponent as ModalLinkedIn } from 'assets/Modal-LinkedIn.svg';
 
 // import Box from '@mui/material/Box';
 // import Typography from '@mui/material/Typography';
