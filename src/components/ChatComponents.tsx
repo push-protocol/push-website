e3670d7ef7870b4caab5b23a7ffa962dad34e662
corlard3y <corlardey@gmail.com>
2023-08-18 18:37:59 +0100

    add condition

--- src/components/ChatComponents.tsx
+++ src/components/ChatComponents.tsx
@@ -3,17 +3,21 @@ import { ChatViewComponent } from '@pushprotocol/uiweb';
 import styled from 'styled-components';
 import { Section } from './SharedStyling';
 import { device } from 'config/globals';
+import { useWeb3React } from '@web3-react/core';
 
 
 
 
 const ChatComponent = () => {
-      
-  return (
-    <ChatViewComponentCard>
-      <ChatViewComponent chatId='24b029b8e07e60291bf9d8c0c48ff993fa1e0a99105459f7404c425c92e91bac' />
-    </ChatViewComponentCard>
-  );
+  const { account, library } = useWeb3React();
+   
+  if(account || library){
+    return (
+      <ChatViewComponentCard>
+        <ChatViewComponent chatId='24b029b8e07e60291bf9d8c0c48ff993fa1e0a99105459f7404c425c92e91bac' />
+      </ChatViewComponentCard>
+    );
+  } else return null;
 };
 
 const ChatViewComponentCard = styled(Section)`
