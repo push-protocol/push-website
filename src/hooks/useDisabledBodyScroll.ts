e511b1f9cb2e736942cb55f5cc69e82b1b8bfcdf
corlard3y <corlardey@gmail.com>
2023-08-21 11:36:20 +0100

    disable scroll fix

--- src/hooks/useDisabledBodyScroll.ts
+++ src/hooks/useDisabledBodyScroll.ts
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 export const useDisableBodyScroll = ({ open }: { open: boolean }) => {
   useEffect(() => {
     if (open) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflowY = 'hidden';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflowY = 'unset';
     }
   }, [open]);
 };
