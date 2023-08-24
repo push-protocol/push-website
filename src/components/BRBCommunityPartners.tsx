f9e7cfb9ffe0f8a5a23d6e41a523c8be5c78cdeb
deekshithepns <deekshith@push.org>
2023-08-24 17:51:23 +0530

    updated speed

--- src/components/BRBCommunityPartners.tsx
+++ src/components/BRBCommunityPartners.tsx
@@ -14,7 +14,7 @@ import { ButtonV2, ImageV2, ItemHV2 } from './SharedStylingV2';
 export const CommunityPartners = () => {
   const isMobile = useMediaQuery(device.mobileL);
 
-  const ANIMATION_SPEED = isMobile ? 42 : 70;
+  const ANIMATION_SPEED = isMobile ? 55 : 70;
 
   return (
     <Container>
