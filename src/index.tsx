701236fc9d92cb5c76edb4138484aec0e3a1c505
corlard3y <corlardey@gmail.com>
2023-08-31 11:11:02 +0100

    no cache

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import './i18n';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { Web3ReactProvider } from '@web3-react/core';
 import { ethers } from 'ethers';
@@ -33,7 +33,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
