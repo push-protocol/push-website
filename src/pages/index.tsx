731ed7897e861565f125c55c3d0d7819114704c1
corlard3y <corlardey@gmail.com>
2023-09-27 14:23:54 +0100

    update styles components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -127,7 +127,7 @@ export default function Home(): JSX.Element {
         </script>
       </Head> */}
       
-      <main className='homewrapper'>
+      <main>
         {/* HERO SECTION */}
         <SectionV2
           id="hero"
@@ -957,30 +957,30 @@ const ResponsiveH2 = styled(H2V2)`
   }
 `;
 
-// const HomeWrapper = styled.main`
-//   margin: 0;
-//   padding: 0;
-//   box-sizing: border-box;
-//   & #hero .contentBox {
-//     row-gap: 18px;
-//   }
+const HomeWrapper = styled.main`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  & #hero .contentBox {
+    row-gap: 18px;
+  }
 
-//   & .pushMissingSvg {
-//     width: 500px;
-//     @media ${device.laptop} {
-//       width: 400px;
-//     }
-//     @media ${device.tablet} {
-//       width: 100%;
-//     }
-//   }
-//   @media ${device.tablet} {
-//     width: 100%;
+  & .pushMissingSvg {
+    width: 500px;
+    @media ${device.laptop} {
+      width: 400px;
+    }
+    @media ${device.tablet} {
+      width: 100%;
+    }
+  }
+  @media ${device.tablet} {
+    width: 100%;
 
-//     & svg.figureSvg {
-//       width: 100%;
-//     }
-//   }
+    & svg.figureSvg {
+      width: 100%;
+    }
+  }
 // `;
 
 /**
@@ -1092,58 +1092,58 @@ const PoweringCommunicationRow = styled(ItemHV2)`
   }
 `;
 
-// const LiveNetworks = styled(ItemHV2)` 
-//   background: #ffffff;
-//   border: 1px solid #bac4d6;
-//   border-radius: 28px;
-//   display: flex;
-//   align-items: center;
-//   align-self: flex-start;
-//   justify-content: space-between;
-//   box-sizing: border-box;
-//   max-width: 550px;
+const LiveNetworks = styled(ItemHV2)` 
+  background: #ffffff;
+  border: 1px solid #bac4d6;
+  border-radius: 28px;
+  display: flex;
+  align-items: center;
+  align-self: flex-start;
+  justify-content: space-between;
+  box-sizing: border-box;
+  max-width: 550px;
 
-//   & .network {
-//     padding: 26px 16px;
+  & .network {
+    padding: 26px 16px;
 
-//     & svg {
-//       width: 106px;
-//       height: 26px;
-//     }
-//   }
+    & svg {
+      width: 106px;
+      height: 26px;
+    }
+  }
 
-//   @media ${device.laptopL} {
-//     flex-direction: row;
-//     flex-wrap: nowrap;
-//     border-radius: 18px;
+  @media ${device.laptopL} {
+    flex-direction: row;
+    flex-wrap: nowrap;
+    border-radius: 18px;
 
-//     & .network {
-//       padding: 26px 16px;
+    & .network {
+      padding: 26px 16px;
 
-//       & svg {
-//         width: 106px;
-//         height: 26px;
-//       }
-//     }
-//   }
+      & svg {
+        width: 106px;
+        height: 26px;
+      }
+    }
+  }
 
-//   @media ${device.tablet} {
-//     flex-direction: row;
-//     flex-wrap: nowrap;
-//     border-radius: 18px;
-//     max-width: initial;
-//     align-self: stretch;
+  @media ${device.tablet} {
+    flex-direction: row;
+    flex-wrap: nowrap;
+    border-radius: 18px;
+    max-width: initial;
+    align-self: stretch;
 
-//     & .network {
-//       padding: 15px 10px;
+    & .network {
+      padding: 15px 10px;
 
-//       & svg {
-//         width: 79.5px;
-//         height: 19.5px;
-//       }
-//     }
-//   }
-// `;
+      & svg {
+        width: 79.5px;
+        height: 19.5px;
+      }
+    }
+  }
+`;
 
 const LiveNetworkCard = styled(ItemVV2)`
   flex: 1;
