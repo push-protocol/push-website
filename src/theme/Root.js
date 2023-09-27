e54043fda5d8acde27b6ed76fc6a559abaef5c6d
harshrajat <harsh@epns.io>
2023-09-27 21:24:35 +0400

    to support adding super imposed conditions in SSR

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -13,58 +13,8 @@ import Header from '@site/src/segments/Header';
 // Initialize Internalization
 i18nInitialize();
 
-// return superimposed class names if conditions are met
-function returnAdditionalClasses(conditions) {
-  let result = '';
-  for(var i = 0; i < conditions.length; i++) {
-    const item = conditions[i];
-
-    if (locationPathExists(item.pathname, item.condition)) {
-      result = item.classname;
-      break;
-    }
-  }
-  
-  // return result
-  return result;
-}
-
-// enable disable default config
-function excludeDefaultConfigAt(pathname, condition) {
-  return !locationPathExists(pathname, condition);
-}
-
-// check if location path exists
-function locationPathExists(pathname, condition, comingfrom = null) {
-  let result = false;
-  pathname = pathname.toUpperCase();
-
-  // Define location
-  const location = useLocation();
-  
-  const str = location.pathname.toUpperCase();
-  const modstr = (str != null && str.length >= pathname.length) ? str.substring(0, pathname.length) : null;
-
-  result = modstr === pathname ? true : false;
-  if (condition === "exact" || condition === "subpaths") {
-
-    // check if sub path exist in pathname and change result;
-    if (str.length > pathname.length && str.substring(0, pathname.length) === pathname) {
-      // path has more 
-      const remainingLength = str.substring(pathname.length, pathname.length + 1) === "/" ? str.length - pathname.length - 1 : str.length - pathname.length;
-      
-      if (condition === "exact") {
-        result = remainingLength == 0 ? true : false;
-      } else if (condition === "subpaths") {
-        result = remainingLength > 0 ? true : false;
-      }
-    }
-  }
-
-  return result;
-}
-
 export default function Root({children}) {
+  // superimposed conditions
   const superimposedConditions = [
     {
       classname: 'DocsHub PushDocs DocusaurusNavbar NavBarDark',
@@ -87,6 +37,57 @@ export default function Root({children}) {
       condition: 'subpaths'
     }
   ];
+
+  // return superimposed class names if conditions are met
+  function returnAdditionalClasses(conditions) {
+    let result = '';
+    for(var i = 0; i < conditions.length; i++) {
+      const item = conditions[i];
+
+      if (locationPathExists(item.pathname, item.condition)) {
+        result = item.classname;
+        break;
+      }
+    }
+    
+    // return result
+    return result;
+  }
+
+  // enable disable default config
+  function excludeDefaultConfigAt(pathname, condition) {
+    return !locationPathExists(pathname, condition);
+  }
+
+  // check if location path exists
+  function locationPathExists(pathname, condition, comingfrom = null) {
+    let result = false;
+    pathname = pathname.toUpperCase();
+
+    // Define location
+    const location = useLocation();
+    
+    const str = location.pathname.toUpperCase();
+    const modstr = (str != null && str.length >= pathname.length) ? str.substring(0, pathname.length) : null;
+
+    result = modstr === pathname ? true : false;
+    if (condition === "exact" || condition === "subpaths") {
+
+      // check if sub path exist in pathname and change result;
+      if (str.length > pathname.length && str.substring(0, pathname.length) === pathname) {
+        // path has more 
+        const remainingLength = str.substring(pathname.length, pathname.length + 1) === "/" ? str.length - pathname.length - 1 : str.length - pathname.length;
+        
+        if (condition === "exact") {
+          result = remainingLength == 0 ? true : false;
+        } else if (condition === "subpaths") {
+          result = remainingLength > 0 ? true : false;
+        }
+      }
+    }
+
+    return result;
+  }
 
   return (
     <div class={returnAdditionalClasses(superimposedConditions)}>
