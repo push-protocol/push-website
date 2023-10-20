2aa557a01e818cde6580d111d35a5c936761e054
harshrajat <harsh@epns.io>
2023-10-20 16:37:12 +0400

    Added webpack polyfill as per barebone push chat example

--- plugins/custom-webpack-plugin/index.js
+++ plugins/custom-webpack-plugin/index.js
@@ -13,33 +13,41 @@ module.exports = function (context, options) {
         resolve: {
           alias: {},
           fallback: {
+            assert: require.resolve("assert/"),
+            buffer: require.resolve('buffer'),
+            child_process: false,
+            constants: require.resolve('constants-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            fs: false,
             http: require.resolve("stream-http"),
             https: require.resolve("https-browserify"),
-            zlib: require.resolve("browserify-zlib"),
-            url: require.resolve("url/"),
+            os: require.resolve('os-browserify/browser'),
+            path: require.resolve('path-browserify'),
+            url: require.resolve('url'),
+            util: require.resolve('util/'),
             stream: require.resolve("stream-browserify"),
-            assert: require.resolve("assert/"),
-            crypto: require.resolve("crypto-browserify"),
-            buffer: require.resolve("buffer/"),
-            util: require.resolve("util/"),
+            zlib: require.resolve("browserify-zlib"),
           },
+          extensions: [
+            '.ts',
+            '.js',
+          ]
         },
         plugins: [
-          // new webpack.ProvidePlugin({
-          //   process: "process/browser.js",
-          //   Buffer: ["buffer", "Buffer"],
-          //   "window.crypto": polyfillPath,
-          // }),
+          new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+          }),
         ],
         module: {
-          // rules: [
-          //   {
-          //     test: /\.m?js/,
-          //     resolve: {
-          //       fullySpecified: false,
-          //     },
-          //   },
-          // ],
+          rules: [
+            {
+              test: /\.m?js/,
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
         },
       };
     },
