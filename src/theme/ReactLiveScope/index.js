b61bef80a75d40ab57ff5ed8ed8cfc9e7b3320c8
harshrajat <harsh@epns.io>
2023-11-03 02:36:25 +0400

    integrating notificaton section

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,27 +1,46 @@
 
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import { default as BrowserOnly } from '@docusaurus/BrowserOnly';
 import React from 'react';
 
+// This function returns a promise that resolves to the library, 
+// ensuring it's only imported on the client side.
+function loadClientSideLibraryEthers(constantName) {
+  return typeof window !== 'undefined'
+    ? require('ethers')[constantName]
+    : Promise.resolve({}); // Return an empty object or appropriate placeholder for SSR.
+}
+
+function loadClientSideLibraryPushProtocolRestAPI(constantName) {
+  return typeof window !== 'undefined'
+    ? require('@pushprotocol/restapi')[constantName]
+    : Promise.resolve({}); // Return an empty object or appropriate placeholder for SSR.
+}
+
+// For @pushprotocol/UIWeb components, we will dynamically load them in the BrowserOnly component.
 function createBrowserOnlyLibComponentUIWeb(componentExportName) {
   return function LibComponentBrowserOnly(props) {
     return (
-      <BrowserOnly fallback={<div>...</div>}>
+      <BrowserOnly fallback={<div>Loading...</div>}>
         {() => {
-          const Component = require("@pushprotocol/uiweb")[componentExportName];
+          const Component = require('@pushprotocol/uiweb')[componentExportName];
           return <Component {...props} />;
         }}
       </BrowserOnly>
     );
   }
-};
+}
 
-// Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
-  NotificationItem: createBrowserOnlyLibComponentUIWeb("NotificationItem"),
-  ChatUIProvider: createBrowserOnlyLibComponentUIWeb("ChatUIProvider"),
-  ChatViewComponent: createBrowserOnlyLibComponentUIWeb("ChatViewComponent"),
-  darkChatTheme: createBrowserOnlyLibComponentUIWeb("darkChatTheme"),
+  // Asynchronously import ethers and PushAPI only on the client side
+  ethers: loadClientSideLibraryEthers('ethers'),
+  PushAPI: loadClientSideLibraryPushProtocolRestAPI('PushAPI'),
+  // Continue using the BrowserOnly component for UI components
+  NotificationItem: createBrowserOnlyLibComponentUIWeb('NotificationItem'),
+  ChatUIProvider: createBrowserOnlyLibComponentUIWeb('ChatUIProvider'),
+  ChatViewComponent: createBrowserOnlyLibComponentUIWeb('ChatViewComponent'),
+  darkChatTheme: createBrowserOnlyLibComponentUIWeb('darkChatTheme'),
 };
+
 export default ReactLiveScope;
