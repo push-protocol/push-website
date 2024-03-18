3e43d51076369e5e0fb591ac122b19f09579df3f
corlard3y <corlardey@gmail.com>
2024-03-18 11:00:03 +0100

    Cooke Modal Opacity

--- src/components/CookieComponent.tsx
+++ src/components/CookieComponent.tsx
@@ -39,7 +39,7 @@ const CookieComponent = () => {
 
   return (
     <>
-    {!cookies.pushCookies && showModal && (
+    {/* {!cookies.pushCookies && showModal && ( */}
     <CookieContainer>
       <H2 
         color="#000"
@@ -57,7 +57,8 @@ const CookieComponent = () => {
           <AcceptButton onClick={handleAccept}>Accept</AcceptButton>
           <RejectButton onClick={handleReject}>Opt-out</RejectButton>
         </ButtonContainer>
-    </CookieContainer>)}
+    </CookieContainer>
+    {/* )} */}
     </>
   );
 };
@@ -77,6 +78,8 @@ const CookieContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  border: 1px solid rgba(0, 0, 0, 0.10);
+  backdrop-filter: blur(12px);
 
   h2 {
 
