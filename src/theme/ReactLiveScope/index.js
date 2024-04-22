6caa9352c06c71c4fbd9bad1ced169dc313f6cdd
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-04-22 17:44:16 +0530

    added support chat v2 docs

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -59,6 +59,7 @@ const ReactLiveScope = {
   ),
   // Continue using the BrowserOnly component for UI components
   SupportChat: createBrowserOnlyLibComponentUIWeb("SupportChat"),
+  SupportChatV2: createBrowserOnlyLibComponentUIWeb("SupportChatV2"),
   NotificationItem: createBrowserOnlyLibComponentUIWeb("NotificationItem"),
   ChatUIProvider: createBrowserOnlyLibComponentUIWeb("ChatUIProvider"),
   ChatView: createBrowserOnlyLibComponentUIWeb("ChatView"),
@@ -71,6 +72,7 @@ const ReactLiveScope = {
   ChatPreview: createBrowserOnlyLibComponentUIWeb("ChatPreview"),
   ChatPreviewList: createBrowserOnlyLibComponentUIWeb("ChatPreviewList"),
   darkChatTheme: createBrowserOnlyLibComponentUIWeb("darkChatTheme"),
+  lightChatTheme: createBrowserOnlyLibComponentUIWeb("lightChatTheme"),
   NotificationItem: createBrowserOnlyLibComponentUIWeb("NotificationItem"),
   SubscriptionManager: createBrowserOnlyLibComponentUIWeb(
     "SubscriptionManager",
