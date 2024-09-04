ac68374bcb2d59ac4a84fd787731127f1e25d554
corlard3y <corlardey@gmail.com>
2024-09-04 12:11:25 +0100

    add deploy prevew file and basepath

---
+++ basePath.js
@@ -0,0 +1,9 @@
+export function getPreviewBasePath() {
+  if (typeof process !== 'undefined' && process.env) {
+    // Node.js environment (e.g., during the build process)
+    return process.env.REACT_APP_PR_PREVIEW_BASE
+      ? `/push-website/pr-preview/${process.env.REACT_APP_PR_PREVIEW_BASE}`
+      : '';
+  }
+  return '';
+}
