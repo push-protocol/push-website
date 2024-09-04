6f51a4d352341913bc69726dfa81f4b8a938692f
corlard3y <corlardey@gmail.com>
2024-09-04 12:29:21 +0100

    update basepath import

--- basePath.js
+++ basePath.js
@@ -1,4 +1,4 @@
-export function getPreviewBasePath() {
+export const getPreviewBasePath = () => {
   if (typeof process !== 'undefined' && process.env) {
     // Node.js environment (e.g., during the build process)
     return process.env.REACT_APP_PR_PREVIEW_BASE
@@ -6,4 +6,4 @@ export function getPreviewBasePath() {
       : '';
   }
   return '';
-}
+};
