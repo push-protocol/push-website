158064be35df7b9ad7ca33c177afca0e72d74ccf
corlard3y <corlardey@gmail.com>
2024-09-11 14:04:05 +0100

    add script to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -141,6 +141,26 @@ export default function Home({ homePageBlogMetadata, recentPosts }) {
             ],
           })}
         </script>
+
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (l) {
+                if (l.search[1] === '/') {
+                  var decoded = l.search
+                    .slice(1)
+                    .split('&')
+                    .map(function (s) {
+                      return s.replace(/~and~/g, '&');
+                    })
+                    .join('?');
+                  window.history.replaceState(null, null, l.pathname.slice(0, -1) + decoded + l.hash);
+                }
+              })(window.location);
+            `,
+          }}
+        />
       </Head>
 
       <HomeWrapper>
