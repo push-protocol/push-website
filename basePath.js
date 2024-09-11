83c364e121b880172b4da6b1be0e33a4bceac72d
corlard3y <corlardey@gmail.com>
2024-09-11 01:36:17 +0100

    update basepath

--- basePath.js
+++ basePath.js
@@ -3,7 +3,7 @@ function getPreviewBasePath() {
   if (typeof process !== 'undefined' && process.env) {
     // Node.js environment (e.g., during the build process)
     return process.env.REACT_APP_PREVIEW_BASE
-      ? `/push-website/pr-preview/${process.env.REACT_APP_PREVIEW_BASE}`
+      ? `/push-website/pr-preview/pr-${process.env.REACT_APP_PREVIEW_BASE}`
       : '';
   }
   return '';
