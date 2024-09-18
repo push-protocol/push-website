0d209da679bd3a158672b76dedb86b2949cf5d0d
corlard3y <corlardey@gmail.com>
2024-09-18 02:44:27 +0100

    update console

--- src/utils/useRouteHelper.js
+++ src/utils/useRouteHelper.js
@@ -1,8 +1,11 @@
-import { getPreviewBasePath } from '../../basePath';
-
-const basePath = getPreviewBasePath();
-
 export const getPublicAssetPath = (path) => {
-  console.log('Base Path:', basePath); // Logs the basePath value to the console
-  return basePath ? `${basePath}${path}` : `${path}`;
+  console.log(
+    'Process.env:',
+    process.env,
+    'process.env.preview-base',
+    process.env.REACT_APP_PREVIEW_BASE
+  ); // Logs the basePath value to the console
+  return process.env.GITHUB_ACTIONS
+    ? `/push-website/pr-preview/${process.env.REACT_APP_PREVIEW_BASE}${path}`
+    : `${path}`;
 };
