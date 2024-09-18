969d8349afaa48a22fd480126f1db128e16bfeea
corlard3y <corlardey@gmail.com>
2024-09-18 03:23:00 +0100

    update os-browserify

--- plugins/custom-webpack-plugin/index.js
+++ plugins/custom-webpack-plugin/index.js
@@ -26,6 +26,7 @@ module.exports = function (context, options) {
             http: require.resolve('stream-http'),
             https: require.resolve('https-browserify'),
             stream: require.resolve('stream-browserify'),
+            os: require.resolve('os-browserify/browser'),
             url: require.resolve('url'),
             zlib: require.resolve('browserify-zlib'),
             crypto: require.resolve('crypto-browserify'),
