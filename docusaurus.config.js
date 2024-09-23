827834b2bb4d35fd70d81d6ccaafa5fcdd371fe5
corlard3y <corlardey@gmail.com>
2024-09-23 22:44:28 +0100

    update head tags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,18 +145,24 @@ const config = {
         type: 'text/javascript',
       },
       innerHTML: `
-       (function (l) {
-          if (l.search[1] === '/') {
-            var decoded = l.search
-              .slice(1)
-              .split('&')
-              .map(function (s) {
-                return s.replace(/~and~/g, '&');
-              })
-              .join('?');
-            window.history.replaceState(null, null, l.pathname.slice(0, -1) + decoded + l.hash);
-          }
-        })(window.location);
+         var isPrPreview = window.location.pathname.includes('/pr-preview/');
+         var pathSegmentsToKeep = isPrPreview ? 3 : 0;
+
+        var l = window.location;
+        l.replace(
+          l.protocol +
+            '//' +
+            l.hostname +
+            (l.port ? ':' + l.port : '') +
+            l.pathname
+              .split('/')
+              .slice(0, 1 + pathSegmentsToKeep)
+              .join('/') +
+            '/?/' +
+            l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+            (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+            l.hash
+          );
       `,
     },
   ],
