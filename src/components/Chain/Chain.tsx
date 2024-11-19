176a614100e81970747a7cf501866db3674aa510
corlard3y <corlardey@gmail.com>
2024-11-19 11:01:00 +0100

    add fonts

--- src/components/Chain/Chain.tsx
+++ src/components/Chain/Chain.tsx
@@ -1,7 +1,44 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React from 'react';
 
+import styled from 'styled-components';
+
+import { device } from '../../../src/config/globals';
+
+import { ItemV } from '../../../src/css/SharedStyling';
+
 const Chain = () => {
-  return <div>Chain</div>;
+  return (
+    <ChainWrapper>
+      {/* Header */}
+      <div>Header</div>
+
+      {/* Footer */}
+      <div>Footer</div>
+    </ChainWrapper>
+  );
 };
 
 export default Chain;
+
+const ChainWrapper = styled(ItemV)`
+  overflow: hidden;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #e8eff8;
+  font-family: N27, sans-serif;
+
+  & .pushMissingSvg {
+    width: 900px;
+    @media ${device.tablet} {
+      width: 50%;
+    }
+    @media ${device.mobileL} {
+      width: 248px;
+    }
+  }
+`;
