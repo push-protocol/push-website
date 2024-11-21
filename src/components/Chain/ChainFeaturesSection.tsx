d150289bd1bf30ef180f7e4ebcf928c2637e64b0
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-11-21 16:21:45 +0530

    ui fixes done

--- src/components/Chain/ChainFeaturesSection.tsx
+++ src/components/Chain/ChainFeaturesSection.tsx
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import { TbArrowUpRight } from 'react-icons/tb';
 import styled from 'styled-components';
 import { Button } from '@site/src/css/SharedStyling';
+import { device } from '../../../src/config/globals';
 
 export type ChainFeaturesSectionProps = {};
 
@@ -159,7 +160,13 @@ const ChainFeaturesContainer = styled.div`
   display: flex;
   padding: 172px 0px 224px 0px;
   flex-direction: column;
-  width: -webkit-fill-available;
+  width: 1200px;
+  margin: 213px auto 0 auto;
+
+  @media (max-width: 1248px) {
+    width: 100%;
+    padding: 0 24px;
+  }
 
   .inverted-divider {
     transform: rotate(180deg);
@@ -179,7 +186,7 @@ const InnerContainer = styled.div`
   width: -webkit-fill-available;
   margin: -2px 0px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     padding: 16px;
   }
 `;
@@ -189,7 +196,7 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     flex-direction: column;
     align-items: center;
     gap: 24px;
@@ -205,7 +212,7 @@ const Header = styled.span`
   line-height: 120%;
   letter-spacing: -0.96px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     text-align: center;
   }
 `;
@@ -216,7 +223,7 @@ const FeatureContainer = styled.div`
   margin-top: 100px;
   flex-direction: column;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     margin-top: 40px;
   }
 `;
@@ -225,7 +232,7 @@ const FeatureSubContainer = styled.div`
   display: flex;
   gap: 16px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     flex-direction: column;
   }
 `;
@@ -242,7 +249,7 @@ const FeatureTextHeading = styled.span`
   display: inline;
   white-space: nowrap;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     font-size: 28px;
 
     .breakpoint {
@@ -263,7 +270,7 @@ const FeatureTextSubHeading = styled.span`
   display: inline;
   white-space: nowrap;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     font-size: 24px;
 
     .breakpoint {
@@ -281,7 +288,7 @@ const FeatureTextDescription = styled.span`
   line-height: 125%; /* 30px */
   letter-spacing: -0.48px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     font-size: 14px;
   }
 `;
@@ -299,7 +306,7 @@ const FinalityContainer = styled.div`
   border-radius: 48px;
   border: 2px solid #fff;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     width: 100%;
     height: 292px;
     padding: 24px;
@@ -312,7 +319,7 @@ const FeatureContainerSegregator = styled.div`
   width: -webkit-fill-available;
   gap: 16px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     flex-direction: column;
   }
 `;
@@ -322,7 +329,7 @@ const FeatureContainerSecondSegregator = styled.div`
   width: -webkit-fill-available;
   gap: 16px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     flex-direction: column;
   }
 `;
@@ -340,7 +347,7 @@ const TxFeeContainer = styled.div`
   border-radius: 48px;
   background: #4b75ff;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     height: 246px;
     display: none;
   }
@@ -348,7 +355,7 @@ const TxFeeContainer = styled.div`
 const TxFeeContainerMobile = styled(TxFeeContainer)`
   display: none;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: flex;
   }
 `;
@@ -363,7 +370,7 @@ const OnboardingContainer = styled.div`
   border-radius: 48px;
   background: #64f6b2;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     width: 100%;
     height: 219px;
     padding: 24px;
@@ -382,7 +389,7 @@ const AnyChainContainer = styled.div`
   border-radius: 48px;
   background: #fc564a;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     width: 100%;
     height: 216px;
     padding: 24px;
@@ -402,14 +409,14 @@ const ScalableContainer = styled.div`
   border-radius: 48px;
   background: #ffe659;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: none;
   }
 `;
 
 const ScalableContainerMobile = styled(ScalableContainer)`
   display: none;
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: flex;
     width: 47%;
     height: 246px;
@@ -429,7 +436,7 @@ const KnowledgeBaseContainer = styled.div`
   border-radius: 48px;
   background: #e492ff;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     width: 100%;
     height: 244px;
     padding: 24px;
@@ -442,28 +449,28 @@ const KnowledgeBaseTextContainer = styled.div`
   align-items: flex-end;
   width: -webkit-fill-available;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     align-items: flex-start;
   }
 `;
 
 const KnowledgeBaseIcon = styled(TbArrowUpRight)`
   display: flex;
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: none;
   }
 `;
 
 const KnowledgeBaseIconMobile = styled(TbArrowUpRight)`
   display: none;
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: flex;
   }
 `;
 
 const StorageAndScalableContainerMobile = styled.div`
   display: none;
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     display: flex;
     gap: 16px;
   }
@@ -476,7 +483,7 @@ const HeaderTwoContainer = styled.div`
   gap: 20px;
   margin-top: 479px;
 
-  @media (max-width: 425px) {
+  @media ${device.tablet} {
     margin-top: 120px;
   }
 `;
