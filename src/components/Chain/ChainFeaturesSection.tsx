811dc845fecc0d0f9e337e0bf8818729911132dc
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-11-21 15:24:20 +0530

    mobile view fixed

--- src/components/Chain/ChainFeaturesSection.tsx
+++ src/components/Chain/ChainFeaturesSection.tsx
@@ -41,60 +41,87 @@ const ChainFeaturesSection: FC<ChainFeaturesSectionProps> = () => {
           </Link>
         </HeaderContainer>
         <FeatureContainer>
-          <FinalityContainer>
-            <FeatureTextHeading>
-              Instant
-              <br />
-              Finality
-            </FeatureTextHeading>
-          </FinalityContainer>
-          <FeatureContainerSegregator>
-            <OnboardingContainer>
-              <FeatureTextSubHeading>
-                Seamless, Instant
+          <FeatureSubContainer>
+            <FinalityContainer>
+              <FeatureTextHeading>
+                Instant
                 <br />
-                Onboarding
-              </FeatureTextSubHeading>
-            </OnboardingContainer>
-            <FeatureContainerSecondSegregator>
-              <TxFeeContainer>
-                <FeatureTextSubHeading style={{ color: 'white' }}>
-                  Cheap
-                  <br />
-                  Storage &<br />
-                  Tx Fee
-                </FeatureTextSubHeading>
-              </TxFeeContainer>
-              <ScalableContainer>
-                <FeatureTextSubHeading>
-                  Infinitely
-                  <br />
-                  Scalable
-                </FeatureTextSubHeading>
-              </ScalableContainer>
-            </FeatureContainerSecondSegregator>
-          </FeatureContainerSegregator>
-          <KnowledgeBaseContainer>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'flex-end',
-                width: '-webkit-fill-available',
-              }}
-            >
-              <FeatureTextHeading style={{ color: '#000000' }}>
-                Explore the
-                <br />
-                Knowledge Base
+                Finality
               </FeatureTextHeading>
-              <FeatureTextDescription>
-                Learn why Push Chain stands out as
+            </FinalityContainer>
+            <FeatureContainerSegregator>
+              <OnboardingContainer>
+                <FeatureTextSubHeading>
+                  Seamless, Instant
+                  <br />
+                  Onboarding
+                </FeatureTextSubHeading>
+              </OnboardingContainer>
+              <FeatureContainerSecondSegregator>
+                <TxFeeContainer>
+                  <FeatureTextSubHeading style={{ color: 'white' }}>
+                    Cheap
+                    <br />
+                    Storage &<br />
+                    Tx Fee
+                  </FeatureTextSubHeading>
+                </TxFeeContainer>
+                <StorageAndScalableContainerMobile>
+                  <TxFeeContainerMobile>
+                    <FeatureTextSubHeading style={{ color: 'white' }}>
+                      Cheap
+                      <br />
+                      Storage &<br />
+                      Tx Fee
+                    </FeatureTextSubHeading>
+                  </TxFeeContainerMobile>
+                  <ScalableContainerMobile>
+                    <FeatureTextSubHeading>
+                      Infinitely
+                      <br />
+                      Scalable
+                    </FeatureTextSubHeading>
+                  </ScalableContainerMobile>
+                </StorageAndScalableContainerMobile>
+                <AnyChainContainer>
+                  <FeatureTextSubHeading style={{ color: 'white' }}>
+                    Any Chain
+                    <br />
+                    Transactions
+                  </FeatureTextSubHeading>
+                </AnyChainContainer>
+              </FeatureContainerSecondSegregator>
+            </FeatureContainerSegregator>
+          </FeatureSubContainer>
+          <FeatureSubContainer>
+            <KnowledgeBaseContainer>
+              <KnowledgeBaseIcon size={56} style={{ alignSelf: 'flex-end' }} />
+              <KnowledgeBaseIconMobile
+                size={24}
+                style={{ alignSelf: 'flex-end' }}
+              />
+              <KnowledgeBaseTextContainer>
+                <FeatureTextHeading style={{ color: '#000000' }}>
+                  Explore the
+                  <br />
+                  Knowledge Base
+                </FeatureTextHeading>
+                <FeatureTextDescription>
+                  Learn why Push Chain stands out as
+                  <br />
+                  the foremost consumer chain in web3
+                </FeatureTextDescription>
+              </KnowledgeBaseTextContainer>
+            </KnowledgeBaseContainer>
+
+            <ScalableContainer>
+              <FeatureTextSubHeading>
+                Infinitely
                 <br />
-                the foremost consumer chain in web3
-              </FeatureTextDescription>
-            </div>
-          </KnowledgeBaseContainer>
+                Scalable
+              </FeatureTextSubHeading>
+            </ScalableContainer>
+          </FeatureSubContainer>
         </FeatureContainer>
 
         <HeaderTwoContainer>
@@ -147,12 +174,22 @@ const InnerContainer = styled.div`
   padding: 167px 120px 409px 120px;
   width: -webkit-fill-available;
   margin: -2px 0px;
+
+  @media (max-width: 425px) {
+    padding: 16px;
+  }
 `;
 
 const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+  }
 `;
 
 const Header = styled.span`
@@ -163,13 +200,30 @@ const Header = styled.span`
   font-weight: 500;
   line-height: 120%;
   letter-spacing: -0.96px;
+
+  @media (max-width: 425px) {
+    text-align: center;
+  }
 `;
 
 const FeatureContainer = styled.div`
   display: flex;
   gap: 16px;
   margin-top: 100px;
-  position: relative;
+  flex-direction: column;
+
+  @media (max-width: 425px) {
+    margin-top: 40px;
+  }
+`;
+
+const FeatureSubContainer = styled.div`
+  display: flex;
+  gap: 16px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
 
 const FeatureTextHeading = styled.span`
@@ -180,6 +234,10 @@ const FeatureTextHeading = styled.span`
   font-weight: 400;
   line-height: 100%; /* 52px */
   letter-spacing: -1.04px;
+
+  @media (max-width: 425px) {
+    font-size: 28px;
+  }
 `;
 
 const FeatureTextSubHeading = styled.span`
@@ -190,6 +248,10 @@ const FeatureTextSubHeading = styled.span`
   font-weight: 400;
   line-height: 100%; /* 36px */
   letter-spacing: -0.72px;
+
+  @media (max-width: 425px) {
+    font-size: 24px;
+  }
 `;
 
 const FeatureTextDescription = styled.span`
@@ -200,6 +262,10 @@ const FeatureTextDescription = styled.span`
   font-weight: 400;
   line-height: 125%; /* 30px */
   letter-spacing: -0.48px;
+
+  @media (max-width: 425px) {
+    font-size: 14px;
+  }
 `;
 
 const FinalityContainer = styled.div`
@@ -214,6 +280,12 @@ const FinalityContainer = styled.div`
   flex-shrink: 0;
   border-radius: 48px;
   border: 2px solid #fff;
+
+  @media (max-width: 425px) {
+    width: 100%;
+    height: 292px;
+    padding: 24px;
+  }
 `;
 
 const FeatureContainerSegregator = styled.div`
@@ -221,17 +293,25 @@ const FeatureContainerSegregator = styled.div`
   flex-direction: column;
   width: -webkit-fill-available;
   gap: 16px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
 
 const FeatureContainerSecondSegregator = styled.div`
   display: flex;
   width: -webkit-fill-available;
   gap: 16px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
 
 const TxFeeContainer = styled.div`
   display: flex;
-  width: 50%;
+  width: 49%;
   height: 278.124px;
   padding: 32px;
   flex-direction: column;
@@ -241,8 +321,19 @@ const TxFeeContainer = styled.div`
   flex-shrink: 0;
   border-radius: 48px;
   background: #4b75ff;
-`;
 
+  @media (max-width: 425px) {
+    height: 246px;
+    display: none;
+  }
+`;
+const TxFeeContainerMobile = styled(TxFeeContainer)`
+  display: none;
+
+  @media (max-width: 425px) {
+    display: flex;
+  }
+`;
 const OnboardingContainer = styled.div`
   display: flex;
   width: 100%;
@@ -253,12 +344,37 @@ const OnboardingContainer = styled.div`
   flex-shrink: 0;
   border-radius: 48px;
   background: #64f6b2;
+
+  @media (max-width: 425px) {
+    width: 100%;
+    height: 219px;
+    padding: 24px;
+  }
+`;
+const AnyChainContainer = styled.div`
+  display: flex;
+  width: 49%;
+  height: 278.124px;
+  padding: 32px;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 10px;
+  flex-shrink: 0;
+  border-radius: 48px;
+  background: #fc564a;
+
+  @media (max-width: 425px) {
+    width: 100%;
+    height: 216px;
+    padding: 24px;
+  }
 `;
 
 const ScalableContainer = styled.div`
   display: flex;
-  width: 50%;
-  height: 709px;
+  width: 24%;
+  height: 413.56px;
   padding: 32px;
   flex-direction: column;
   justify-content: flex-end;
@@ -267,22 +383,72 @@ const ScalableContainer = styled.div`
   flex-shrink: 0;
   border-radius: 48px;
   background: #ffe659;
+
+  @media (max-width: 425px) {
+    display: none;
+  }
+`;
+
+const ScalableContainerMobile = styled(ScalableContainer)`
+  display: none;
+  @media (max-width: 425px) {
+    display: flex;
+    width: 47%;
+    height: 246px;
+  }
 `;
 
 const KnowledgeBaseContainer = styled.div`
   display: flex;
-  width: 75.5%;
+  width: 75.2%;
   height: 413.56px;
   padding: 40px;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: flex-start;
   gap: 10px;
   flex-shrink: 0;
   border-radius: 48px;
   background: #e492ff;
-  position: absolute;
-  bottom: 0;
+
+  @media (max-width: 425px) {
+    width: 100%;
+    height: 244px;
+    padding: 24px;
+  }
+`;
+
+const KnowledgeBaseTextContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: -webkit-fill-available;
+
+  @media (max-width: 425px) {
+    align-items: flex-start;
+  }
+`;
+
+const KnowledgeBaseIcon = styled(TbArrowUpRight)`
+  display: flex;
+  @media (max-width: 425px) {
+    display: none;
+  }
+`;
+
+const KnowledgeBaseIconMobile = styled(TbArrowUpRight)`
+  diplay: none;
+  @media (max-width: 425px) {
+    display: flex;
+  }
+`;
+
+const StorageAndScalableContainerMobile = styled.div`
+  display: none;
+  @media (max-width: 425px) {
+    display: flex;
+    gap: 16px;
+  }
 `;
 
 const HeaderTwoContainer = styled.div`
@@ -291,6 +457,10 @@ const HeaderTwoContainer = styled.div`
   flex-direction: column;
   gap: 20px;
   margin-top: 479px;
+
+  @media (max-width: 425px) {
+    margin-top: 120px;
+  }
 `;
 
 const HeaderTwo = styled.span`
