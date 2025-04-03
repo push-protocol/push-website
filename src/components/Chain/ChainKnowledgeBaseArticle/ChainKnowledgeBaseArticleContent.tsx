38e51a6729b411a5420234919472d404e45339ec
corlard3y <corlardey@gmail.com>
2025-04-03 16:09:34 +0100

    fix: remove margin

--- src/components/Chain/ChainKnowledgeBaseArticle/ChainKnowledgeBaseArticleContent.tsx
+++ src/components/Chain/ChainKnowledgeBaseArticle/ChainKnowledgeBaseArticleContent.tsx
@@ -205,7 +205,7 @@ const ChainKnowledgeGrid = styled.div`
 
 const TextItem = styled.div`
   max-width: calc(100% - 400px);
-  margin: 20px 0 20px auto;
+  margin: 0 0 0 auto;
   width: inherit;
   color: #000;
 
