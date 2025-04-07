b1c9416fa078e5a8a24749817523a49a0d2324f3
corlard3y <corlardey@gmail.com>
2025-04-07 23:00:42 +0100

    clear unused code

--- src/components/Chain/config/ChainKnowledgeBaseConfig.ts
+++ src/components/Chain/config/ChainKnowledgeBaseConfig.ts
@@ -6,31 +6,17 @@
 //   - `content`: Use HTML or Markdown to create the article (similar to the blog section).
 //   - `url`: For external links, provide the full URL instead of using `content`.
 //NOTE: , cause we use a plugin to get this to work, you have to restart your local server to see the code changes after editing a blog article(TODO: add hot-reload functionality)
+
 import { push101Content } from '../content/push101';
 import { deepDivesContent } from '../content/deepdives';
 import { faqContent } from '../content/faq';
+import { devDocsContent } from '../content/devdocs';
+import { tokenomicsContent } from '../content/tokenomics';
 
 export const knowledgeBaseArticleContent = [
   push101Content,
   deepDivesContent,
-  {
-    slug: 'devdocs',
-    image: 'knowledge_devdocs',
-    title: 'Developer Docs',
-    subtitle: `Let's come straight to the point! I am universal chain... and you are a dev, let's make it official!`,
-    url: '/docs',
-    target: '_self',
-    ctatitle: 'Build Now',
-  },
-
-  {
-    slug: 'tokenomics',
-    image: 'knowledge_tokenomics',
-    title: 'Explore Tokenomics',
-    subtitle: `Learn how Push Chain's tokenomics incentivizes all actors and ensures sustainable network that's built to last`,
-    url: '/blog/push-chain-tokenomics/',
-    target: '_self',
-    ctatitle: 'Learn Token Design',
-  },
+  devDocsContent,
+  tokenomicsContent,
   faqContent,
 ];
---
+++ src/components/Chain/content/tokenomics.js
@@ -0,0 +1,9 @@
+export const tokenomicsContent = {
+  slug: 'tokenomics',
+  image: 'knowledge_tokenomics',
+  title: 'Explore Tokenomics',
+  subtitle: `Learn how Push Chain's tokenomics incentivizes all actors and ensures sustainable network that's built to last`,
+  url: '/blog/push-chain-tokenomics/',
+  target: '_self',
+  ctatitle: 'Learn Token Design',
+};
